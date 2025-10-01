bce94c0317cbeb4dc90f2df1c6890c92f915c75c
git-ramon <ramonrodriguesnw@gmail.com>
2025-10-01 15:16:17 -0300

    import Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,19 +8,6 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
-import Vue from 'vue';
-/* importando e configurando o vuex*/
-import Vuex from 'Vuex'
-
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-    state: {
-        item: {},
-        transacao: { status: '', mensagem: '', dados: '' }
-    }
-})
-
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -33,15 +20,6 @@ const store = new Vuex.Store({
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('login-component', require('./components/Login.vue').default);
-Vue.component('home-component', require('./components/Home.vue').default);
-Vue.component('marcas-component', require('./components/Marcas.vue').default);
-Vue.component('input-container-component', require('./components/InputContainer.vue').default);
-Vue.component('table-component', require('./components/Table.vue').default);
-Vue.component('card-component', require('./components/Card.vue').default);
-Vue.component('modal-component', require('./components/Modal.vue').default);
-Vue.component('alert-component', require('./components/Alert.vue').default);
-Vue.component('paginate-component', require('./components/Paginate.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -49,27 +27,6 @@ Vue.component('paginate-component', require('./components/Paginate.vue').default
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.filter('formataDataTempoGlobal', function(d) {
-    if(!d) return ''
-
-    d = d.split('T')
-
-    let data = d[0]
-    let tempo = d[1]
-
-    //formatando a data
-    data = data.split('-')
-    data = data[2] + '/' + data[1] + '/' + data[0]
-
-    //formatar o tempo
-    tempo = tempo.split('.')
-    tempo = tempo[0]
-
-    return data + ' ' + tempo
-})
-
 const app = new Vue({
     el: '#app',
-    store
 });
-
